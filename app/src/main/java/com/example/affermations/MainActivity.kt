a5074eea42d771eabaac51b36252f274ffd61eaa
ishjaiswal7 <92149715+ishjaiswal7@users.noreply.github.com>
2022-11-02 07:43:38 +0530

    second commit

--- app/src/main/java/com/example/affermations/MainActivity.kt
+++ app/src/main/java/com/example/affermations/MainActivity.kt
@@ -2,13 +2,26 @@ package com.example.affermations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.affermations.adapters.MovieAdapter
 import com.example.affermations.databinding.ActivityMainBinding
+import com.example.affermations.datasources.DataSource
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val movies = DataSource().loadMovies()
+        val adapter = MovieAdapter(
+            this,
+            movies,
+            R.layout.simple_row_layout
+        )
+        binding.movieList.layoutManager = LinearLayoutManager(this)
+        binding.movieList.adapter = adapter
+
     }
 }
--- app/src/main/java/com/example/affermations/adapters/MovieAdapter.kt
+++ app/src/main/java/com/example/affermations/adapters/MovieAdapter.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.affermations.R
 import com.example.affermations.models.Movie
 
 
@@ -12,21 +14,25 @@ class MovieAdapter(
     private val context: Context,
     private val movies: List<Movie>,
     private val layoutResId: Int
-) : RecyclerView.Adapter<MovieAdapter.MovieHolder>(){
+) : RecyclerView.Adapter<MovieAdapter.MovieHolder>() {
 
     class MovieHolder(
-        val itemView: View) :
+        private val itemView: View
+    ) :
         RecyclerView.ViewHolder(itemView) {
-        // todo: bind data to view
+        private val movieText1: TextView = itemView.findViewById(R.id.movieText1)
+        fun bind(movie: Movie) {
+            movieText1.setText(movie.movieResId)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieHolder {
-        val view = LayoutInflater.from(context).inflate(layoutResId,parent, false)
+        val view = LayoutInflater.from(context).inflate(layoutResId, parent, false)
         return MovieHolder(view)
     }
 
     override fun onBindViewHolder(holder: MovieHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(movies[position])
     }
 
     override fun getItemCount() = movies.size
